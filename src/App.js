6d3054d5ffa2fe2c786cda4681cb755cd4e75a3f
Francis Chen <fncis.chen@gmail.com>
2019-02-25 13:48:40 -0800

    adding Route components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { Home, About, Contact, Navigation } from './components';
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/About" component={About} />
+    <Route path="/Contact" component={Contact} />
   </div>
 );
 
